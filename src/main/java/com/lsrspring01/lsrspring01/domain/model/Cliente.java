51b9a533f62b1e652ad4799492bfa7c4694feac2
Rossitto <lucas74rossi@gmail.com>
2020-06-19 21:56:11 -0300

    getters and setters for Cliente.java

---
+++ src/main/java/com/lsrspring01/lsrspring01/domain/model/Cliente.java
@@ -0,0 +1,42 @@
+package com.lsrspring01.lsrspring01.domain.model;
+
+public class Cliente {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
--- src/main/resources/db/migration/v20200619-create_table.sql
+++ src/main/resources/db/migration/v20200619-create_table.sql
@@ -1,13 +1,7 @@
-create table cliente
-(
-    id bigint not null
-    auto_increment,
-    nome varchar
-    (60) not null,
-    mail varchar
-    (255) not null,
-    telefone varchar
-    (20) not null,
-    primary key
-    (id)
+create table if not exists cliente (
+    id bigint not null auto_increment,
+    nome varchar (60) not null,
+    mail varchar (255) not null,
+    telefone varchar (20) not null,
+    primary key (id)
 );
